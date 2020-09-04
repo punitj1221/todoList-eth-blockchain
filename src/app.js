fe8852a10bee0d41149d73df3654754cf355d731
Punit Jain <punitj1221@gmail.com>
2020-09-04 21:17:57 +0530

    4. Completed Tasks

--- src/app.js
+++ src/app.js
@@ -78,7 +78,7 @@ App ={
             $newTaskTemplate.find('input')
                             .prop('name',taskId)
                             .prop('checked',taskCompleted)
-                            // .on('click',App.toggleCompleted)
+                            .on('click',App.toggleCompleted)
             
             if(taskCompleted){
                 $('#completedTaskList').append($newTaskTemplate)
@@ -94,6 +94,12 @@ App ={
         App.setLoading(true)
         const content = $('#newTask').val()
         await App.todoList.createTask(content);
+        window.location.reload();
+    },
+    toggleCompleted: async(e) => {
+        App.setLoading(true)
+        const taskId = e.target.name
+        await App.todoList.toggleCompleted(taskId)
         window.location.reload();
     },
     setLoading: (boolean) => {
